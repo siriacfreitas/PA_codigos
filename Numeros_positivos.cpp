4403e99a7a435a4bb2db7920bcead1f2eba4595c
Síria Cabral <123579270+siriacfreitas@users.noreply.github.com>
2024-09-17 13:59:34 -0300

    Update Numeros_positivos.cpp
    Mudanças feitas comparando o meu código anterior com o do professor

--- Numeros_positivos.cpp
+++ Numeros_positivos.cpp
@@ -2,21 +2,32 @@
 
 using namespace std;
 
-int main()
+int main(void) // pq esse void ?
 {
-    float  numero_escrito;
-    float numeros_guardados=0;
+    double  numero_escrito;
+    double numeros_guardados=0;
     int  escritas=0;
     do{
         cout << "Escreva um número:";
         cin >> numero_escrito;
-        if(numero_escrito >=0){
+        if(numero_escrito >=0.0){
         numeros_guardados += numero_escrito;
         escritas ++;
         }
-    } while (numero_escrito >=0);
+    } while (numero_escrito >=0.0);
+
+     if (escritas==0)
+  {
+    // cerr: stream normalmente utilizada para msgs de erro
+    cerr << "Nao foi digitado nenhum valor. Impossivel calcular media"
+         << endl;
+    return 1; // pq retornar 1?
+
+  }
+
     cout << "A média dos valores positivos e nulos é:";
     cout << (numeros_guardados/escritas);
+
 
     return 0;
 }
